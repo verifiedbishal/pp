ca5ea19b563414c37b239fcafac53e091bdefc4b
Bishal Shrestha <verifiedbishal@gmail.com>
2020-05-02 12:20:18 +0545

    using js to upload files

---
+++ pages/materials/assets/mat/src/ioedata.js
@@ -0,0 +1,109 @@
+
+let ioe1,ioe2,ioe3,ioe4,ioe5,ioe6,ioe7,ioe8,ioe9,ioe10,ioe11,ioe12,ioe13,ioe14,ioe15,ioe16,ioe17,ioe18,ioe19,ioe20 = [];
+
+let h = [
+    {"text" : "Heading of the books"},
+    {"text" : "Advanced Electronics"},
+    {"text" : "Basic Electrical Engineering"},
+    {"text" : "C Programming"},
+    {"text" : "Computer Graphics"},
+    {"text" : "Computer Organization & Architecture"},
+    {"text" : "Control System"},
+    {"text" : "Digital Logic"},
+    {"text" : "Discrete Mathematics"},
+    {"text" : "Electromagnetics"},
+    {"text" : "Electronics Devices & Circuits"},
+    {"text" : "Engineering Chemistry"},
+    {"text" : "Instrumentation II"},
+    {"text" : "Numerical Methods"},
+    {"text" : "Object Oriented Programming"},
+    {"text" : "Probability & Statistics"},
+    {"text" : "Thermodynamics"}
+];
+ioe1 = [
+    {"name" : ["Introduction To Power Electronics","https://drive.google.com/open?id=1RmNmdfOuIUHGo03jbwQ4sD2XZr7QCo0Q"]}
+];
+ioe2 = [
+    {"name" : ["A Text Book of Electrical Technology Vol 1","https://drive.google.com/file/d/1XoiViAlRc46iMzxRIkGQ-WjJ83kQ9fVx"]},
+    {"name" : ["A Text Book of Electrical Technology Vol 2","https://drive.google.com/file/d/14vV9TrCnRum1ou3I0vEda1jwJTzyaMoT"]},
+    {"name" : ["A Text Book of Electrical Technology Vol 3","https://drive.google.com/file/d/1eA5ZkUZEYUNi3ql7JSTu8CQ36EOhZT6m"]},
+    {"name" : ["A Text Book of Electrical Technology Vol 4","https://drive.google.com/file/d/1XUJjRQdMj57MXhy2LFiphP2B6CWnB368/view"]}
+];
+ioe3 = [
+    {"name" : ["Let Us C","https://drive.google.com/open?id=1YKYjo-oBaq2RzdP5j5NdDbew6wDtoPAR"]},
+    {"name" : ["Learning C By Examples","https://drive.google.com/open?id=1wUJUZaTpIiXkVlD3OPx7ryiG-yevrSXG"]},
+    {"name" : ["Numerical Recipies In C","https://drive.google.com/open?id=1w0A8ZMbPWHrw7KpU7f6pei9bSRs3cEFb"]},
+    {"name" : ["The C Programming Language","https://drive.google.com/open?id=1aqqCMkGIHyjFVL2PGDvXb8K8GTEI7dbL"]},
+    {"name" : ["Theoritical Introduction To Programming","https://drive.google.com/open?id=1xXnwCtGiADMKzn1TrJy9q9MgMMw1yP-Q"]},
+    {"name" : ["C How To Program","https://drive.google.com/open?id=11S9jfVAZ2ozhQH6yStqSw8BbfapjoAQ1"]}
+];
+ioe4 = [
+    {"name" : ["Computer Graphics","https://drive.google.com/open?id=1rv0K8xA5iS38aV9BMSbGZ_7BmSERXpnd"]}
+];
+ioe5 = [
+    {"name" : ["COA","https://drive.google.com/open?id=12AOHi1q5-gV6o9EtYylkDY0RKKLWX0qV"]},
+    {"name" : ["Computer Organization & Architecture","https://drive.google.com/open?id=1pYAaj-fRBpTDj4Ih2eq5Qv_dvzEZ78Ju"]}
+];
+ioe6 = [
+    {"name" : ["Modern Control Engineering","https://drive.google.com/open?id=14wwokVvSx2Z7_Dhh1cZmTfRjbJc0L8jj"]},
+    {"name" : ["Root Locus Analysis & Design","https://drive.google.com/open?id=1kkhNWHbcLmgzbWWz8TSygyQ48_MvXZh_"]},
+    {"name" : ["Schaum S. Outlines Feedback & COntrol System","https://drive.google.com/open?id=1h2DWEwRQAWkSxqEXz0wJo2xB-5mFkO3Z"]},
+    {"name" : ["Control System Engineering By Lohit Sahoo","https://drive.google.com/open?id=1rKi93R684_y8-nc66M4SegdGd4M3vY5k"]}
+];
+ioe7 = [
+    {"name" : ["Digital Design - Morris Mano","https://drive.google.com/open?id=1xC9DI1nQO05jsECza8lGN4jz76rux891"]},
+    {"name" : ["Fundamentals Of Digital Electroinics","https://drive.google.com/open?id=1RnvHD_uKcUY3sJkQ4E-u5HjLg_m2SOzy"]},
+    {"name" : ["Lecture Notes For Digital Electronics","https://drive.google.com/open?id=1jgE4FtzOWWYWiN-ilgyPPdjFPVmhZulz"]}
+];
+ioe8 = [
+    {"name" : ["Discrete Mathematics And Its Application","https://drive.google.com/open?id=1DvRaZ6loCCnJKloArUvW2OBByy4rmRPA"]},
+    {"name" : ["Johnsonbaugh, Richard Discrete Mathematics, Instructor's Manual. Sixth Edition. New Jersey, Prentice Hall, 2005","https://drive.google.com/open?id=1RX2xkZb8RfLgqL_Eoskf1e8ZrXT0Nq6D"]}
+];
+ioe9 = [
+    {"name" : ["Electromagnetics By Hayt","https://drive.google.com/open?id=1AWMkPjH9qgTfnieQ1wB4Ymio7N5P2c45"]}
+];
+ioe10 = [
+    {"name" : ["Electronics Devices & Circuits - Boylestad","https://drive.google.com/open?id=1EJNIkmZ0s3BwyRFadzFBFfUFnAQhMVVQ"]},
+    {"name" : ["Microelectronics Circuits - Muhammad H. Rashid","https://drive.google.com/open?id=1FmS2zWsE6din9s6QDjvxoP3imRcaVZ2M"]}
+];
+ioe11 = [
+    {"name" : ["Engineering Chemistry","https://drive.google.com/open?id=1deobg9Q-UNcBsEtIe7d-fZh9rOJSD8vw"]},
+    {"name" : ["Engineering Chemistry All Chapters","https://drive.google.com/open?id=1NS0W3rSE7YBjGKg9INpinhU0B1ymI2lD"]}
+];
+ioe12 = [
+    {"name" : ["Intrumentation II All Chapters","https://drive.google.com/open?id=16Z416e5zOEm7NFsiB2ymj75FCbA9oQ3y"]}
+];
+ioe13 = [
+    {"name" : ["Applied Numerical Methods","https://drive.google.com/open?id=1aeHMTQCcDBD8j841b3X6SSSpVFvx8Dse"]}
+];
+ioe14 = [
+    {"name" : ["C++ How To Program","https://drive.google.com/open?id=1u1H2hF60M9N_47w1cJDVuvbUsBPZrMTr"]},
+    {"name" : ["Jesse Liberty Unleased","https://drive.google.com/open?id=1Mo92cp3eir7UzI6sGdc2oVXjWyKVnwkv"]},
+    {"name" : ["Robert Lafore - OOP","https://drive.google.com/open?id=1aHNQjENq13ro24FlymI0wxwQpTazX9o3"]}
+];
+ioe15 = [
+    {"name" : ["Probability & Random Processes","https://drive.google.com/open?id=1x4_kEPMAvR_0BsWeTcepnDpkavt4a6jP"]},
+    {"name" : ["Probability & Statistics For Engineers","https://drive.google.com/open?id=1NanQgWYMhlVXuMASFHXsnRJNyV2IaTEV"]}
+];
+ioe16 = [
+    {"name" : ["Heat Transfer - A Practical Approach","https://drive.google.com/open?id=1to07gF31s2rPSxKUWJkBPgnWA83cpHUC"]},
+    {"name" : ["Thermodynamics By Yunus And Michael","https://drive.google.com/open?id=1qPebXWbOxWNrduhrH3cpEOKUhvBWe6Sn"]}
+];
+
+    var aa = [ioe1,ioe2,ioe3,ioe4,ioe5,ioe6,ioe7,ioe8,ioe9,ioe10,ioe11,ioe12,ioe13,ioe14,ioe15,ioe16];
+    for(let i = 1; i <= aa.length; i++){
+        var data = document.getElementById("myD" + i);
+        var m = document.createElement("h2");
+        m.innerHTML = '<h2>' + h[i].text + '<span>[Click to view / download]</span></h2>';
+        data.appendChild(m);
+    }
+    for(let j = 1; j <= aa.length; j++){
+        var dataAll = document.getElementById("myD" + j);
+        for(let k = 0; k < aa[j - 1].length ; k++){
+            var el2 = document.createElement("li");
+            el2.innerHTML = '<a href = \"'  + aa[j - 1][k].name[1] + ' \">' + aa[j - 1][k].name[0] + '</a>';
+            dataAll.appendChild(el2);
+        }
+    }
+
+
