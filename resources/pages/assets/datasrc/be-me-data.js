04feaddf4e5ff8c17fc265ef78ff183e0dbd3c12
Bishal Shrestha <verifiedbishal@gmail.com>
2020-05-06 12:32:24 +0545

    huge changes

--- resources/pages/assets/datasrc/be-me-data.js
+++ resources/pages/assets/datasrc/be-me-data.js
@@ -1,6 +1,17 @@
 
 let data = [
     {
+        "heading" : "SYLLABUS",
+        "innerdata" :[
+            ["BCT All",""],
+            ["BCE All",""],
+            ["BEX All",""],
+            ["BME All",""],
+            ["BAG All",""],
+            ["B. Arch All",""]
+        ]
+    },
+    {
         "heading" : "Advanced Electronics",
         "innerdata" : [
             ["Introduction To Power Electronics","https://drive.google.com/open?id=1RmNmdfOuIUHGo03jbwQ4sD2XZr7QCo0Q"]
@@ -49,6 +60,26 @@ let data = [
         ]
     },
     {
+        "heading" : "Communication System I",
+        "innerdata" : [
+            ["An Introduction to Analog and Digital Communications, 2nd Edition by Simon Haykin","https://drive.google.com/open?id=1N4F796z4OmS4K8CdkRlQDV4RQd-CdVC-"],
+            ["Communication Systems - 4 Edition - Haykin","https://drive.google.com/open?id=1df0IsI7mTvWg6EF89oLq8-6o7j2vdZAL"],
+            ["Modern Digital And Analog Communication Systems - B.P.Lathi","https://drive.google.com/open?id=1mQLCnhEN8u5QSMB8l7Xvvt99B5lQoLN7"]
+        ]
+    },
+    {
+        "heading" : "Computer Network",
+        "innerdata" : [
+            ["Computer Networks - A Tanenbaum - 5th edition","https://drive.google.com/open?id=10wpbYPEaNiSJJ0h6Jm-6zXM2J8R39aYP"],
+            ["Computer Networks, 4th edition","https://drive.google.com/open?id=1VGH8qWDipUVsoZFvIUkpIxNESCCvmw0v"],
+            ["Data.And.Computer.Communications.8e.WilliamStallings","https://drive.google.com/open?id=1CW7MItr77e4uXawEt5to776gUcqjRAe2"],
+            ["Curose-Ross - Computer Networking A Top down Approach Featuring the Internet 3rd Edition","https://drive.google.com/open?id=17r_LmkmzOHbGaenV50X-EJlbP_R3NnPO"],
+            ["Computer Networks","https://drive.google.com/open?id=1zbNSCqOzWfrWvDJHaMddFPmM1TTxfpx6"],
+            ["Computer Networking Principles Bonaventure","https://drive.google.com/open?id=1C7R2vDpXhMbaASTWsPnpVqx0ks7Wut1_"],
+            ["Larry L. Peterson, Bruce S. Davie Computer Networks A Systems Approach, Fifth Edition    2011","https://drive.google.com/open?id=1046ygox0veqyPjhenZvxSplkhv5lZEgz"]
+        ]
+    },
+    {
         "heading" : "Digital Logic",
         "innerdata" : [
             ["Digital Design - Morris Mano","https://drive.google.com/open?id=1xC9DI1nQO05jsECza8lGN4jz76rux891"],
@@ -74,6 +105,33 @@ let data = [
         "innerdata" : [
             ["Electronics Devices & Circuits - Boylestad","https://drive.google.com/open?id=1EJNIkmZ0s3BwyRFadzFBFfUFnAQhMVVQ"],
             ["Microelectronics Circuits - Muhammad H. Rashid","https://drive.google.com/open?id=1FmS2zWsE6din9s6QDjvxoP3imRcaVZ2M"]
+        ]
+    },
+    {
+        "heading" : "Embedded System",
+        "innerdata" : [
+            ["AVR Microcontroller and Embedded Systems_vol_1","https://drive.google.com/open?id=1nhyqmDrVMSHVFbJTwb1UjtoID-6O1bPf"],
+            ["All Chapters","https://drive.google.com/open?id=1KFfDnw2-sgtuK_I6Ld-JGUOxnOM0Poqk"],
+            ["Digital Systesm Design Using VHDL Roth","https://drive.google.com/open?id=1PjlRpAYXv46txB8ynaahM8KW38uv9190"],
+            ["digital vlsi design with verilog - John Williams","https://drive.google.com/open?id=1FGbLEZm-CCUa-oviCkrnE5E7s8WomSdg"],
+            ["Embedded System - Surendra Shrestha","https://drive.google.com/open?id=1jkYzLs3AF5FbHxycIJZQIX6J05wlikUs"],
+            ["Embedded System Design-Solution Manual","https://drive.google.com/open?id=17DJWpI3DRq3QmS-F36V6E3DvWJ7CzTLY"],
+            ["IIT Kharagpur","https://drive.google.com/open?id=1rGC6vpXeiNiPbu0az22Env8gWFlpdgvA"],
+            ["ISE Quick Start Tutorial","https://drive.google.com/open?id=1KLIUKBUrQAWRu4dgDxVymKUEMVIKwC6E"],
+            ["PIC Microcontroller and Embedded Systems - Mazidi","https://drive.google.com/open?id=1gn7eK_K9Wme7kEHbCgjsc_uuN0_fp_au"],
+            ["S3EStarter user Guide","https://drive.google.com/open?id=1ZZL4yQSmTH6kmAEWc-PphwWHigMktjFZ"],
+            ["Verilog Reference Guide","https://drive.google.com/open?id=1igxy9ajNuIdyJrS84nRIHT3-AYbYkpBx"],
+            ["VHDL Coding and Logic Synthesis with Synopsys","https://drive.google.com/open?id=1M6FFZlvXz5mC129xdh2-uOh-ee_8DA6M"],
+            ["VHDL Tutorial","https://drive.google.com/open?id=1FEplgKX4DxKwZJUW_8k2nSsvYrMALTeP"]
+        ]
+    },
+    {
+        "heading" : "Engineering Economics",
+        "innerdata" : [
+            ["Engineering Economics","https://drive.google.com/open?id=1RfBgu_QcDdEz3NPmk-KCZGkdMmOXQEaE"],
+            ["Selected Notes - A. M. Clausing","https://drive.google.com/open?id=17n5IKZDydvZiPC4wWZu9i_GnsehcfS1k"],
+            ["Engineering Economics Text Book","https://drive.google.com/open?id=1pc9vpRUd3TCoB9JsDpw6-dj7cR30fZ_5"],
+            ["Impacts of Inflation on Capital Budgeting Decicions","https://drive.google.com/open?id=1b3qvtIW26TOW2YW6gFD5KIrbAodPVJVt"]
         ]
     },
     {
@@ -111,60 +169,6 @@ let data = [
         ]
     },
     {
-        "heading" : "Thermodynamics",
-        "innerdata" : [
-            ["Heat Transfer - A Practical Approach","https://drive.google.com/open?id=1to07gF31s2rPSxKUWJkBPgnWA83cpHUC"],
-            ["Thermodynamics By Yunus And Michael","https://drive.google.com/open?id=1qPebXWbOxWNrduhrH3cpEOKUhvBWe6Sn"]
-        ]
-    },
-    {
-        "heading" : "Communication System I",
-        "innerdata" : [
-            ["An Introduction to Analog and Digital Communications, 2nd Edition by Simon Haykin","https://drive.google.com/open?id=1N4F796z4OmS4K8CdkRlQDV4RQd-CdVC-"],
-            ["Communication Systems - 4 Edition - Haykin","https://drive.google.com/open?id=1df0IsI7mTvWg6EF89oLq8-6o7j2vdZAL"],
-            ["Modern Digital And Analog Communication Systems - B.P.Lathi","https://drive.google.com/open?id=1mQLCnhEN8u5QSMB8l7Xvvt99B5lQoLN7"]
-        ]
-    },
-    {
-        "heading" : "Computer Network",
-        "innerdata" : [
-            ["Computer Networks - A Tanenbaum - 5th edition","https://drive.google.com/open?id=10wpbYPEaNiSJJ0h6Jm-6zXM2J8R39aYP"],
-            ["Computer Networks, 4th edition","https://drive.google.com/open?id=1VGH8qWDipUVsoZFvIUkpIxNESCCvmw0v"],
-            ["Data.And.Computer.Communications.8e.WilliamStallings","https://drive.google.com/open?id=1CW7MItr77e4uXawEt5to776gUcqjRAe2"],
-            ["Curose-Ross - Computer Networking A Top down Approach Featuring the Internet 3rd Edition","https://drive.google.com/open?id=17r_LmkmzOHbGaenV50X-EJlbP_R3NnPO"],
-            ["Computer Networks","https://drive.google.com/open?id=1zbNSCqOzWfrWvDJHaMddFPmM1TTxfpx6"],
-            ["Computer Networking Principles Bonaventure","https://drive.google.com/open?id=1C7R2vDpXhMbaASTWsPnpVqx0ks7Wut1_"],
-            ["Larry L. Peterson, Bruce S. Davie Computer Networks A Systems Approach, Fifth Edition    2011","https://drive.google.com/open?id=1046ygox0veqyPjhenZvxSplkhv5lZEgz"]
-        ]
-    },
-    {
-        "heading" : "Embedded System",
-        "innerdata" : [
-            ["AVR Microcontroller and Embedded Systems_vol_1","https://drive.google.com/open?id=1nhyqmDrVMSHVFbJTwb1UjtoID-6O1bPf"],
-            ["All Chapters","https://drive.google.com/open?id=1KFfDnw2-sgtuK_I6Ld-JGUOxnOM0Poqk"],
-            ["Digital Systesm Design Using VHDL Roth","https://drive.google.com/open?id=1PjlRpAYXv46txB8ynaahM8KW38uv9190"],
-            ["digital vlsi design with verilog - John Williams","https://drive.google.com/open?id=1FGbLEZm-CCUa-oviCkrnE5E7s8WomSdg"],
-            ["Embedded System - Surendra Shrestha","https://drive.google.com/open?id=1jkYzLs3AF5FbHxycIJZQIX6J05wlikUs"],
-            ["Embedded System Design-Solution Manual","https://drive.google.com/open?id=17DJWpI3DRq3QmS-F36V6E3DvWJ7CzTLY"],
-            ["IIT Kharagpur","https://drive.google.com/open?id=1rGC6vpXeiNiPbu0az22Env8gWFlpdgvA"],
-            ["ISE Quick Start Tutorial","https://drive.google.com/open?id=1KLIUKBUrQAWRu4dgDxVymKUEMVIKwC6E"],
-            ["PIC Microcontroller and Embedded Systems - Mazidi","https://drive.google.com/open?id=1gn7eK_K9Wme7kEHbCgjsc_uuN0_fp_au"],
-            ["S3EStarter user Guide","https://drive.google.com/open?id=1ZZL4yQSmTH6kmAEWc-PphwWHigMktjFZ"],
-            ["Verilog Reference Guide","https://drive.google.com/open?id=1igxy9ajNuIdyJrS84nRIHT3-AYbYkpBx"],
-            ["VHDL Coding and Logic Synthesis with Synopsys","https://drive.google.com/open?id=1M6FFZlvXz5mC129xdh2-uOh-ee_8DA6M"],
-            ["VHDL Tutorial","https://drive.google.com/open?id=1FEplgKX4DxKwZJUW_8k2nSsvYrMALTeP"]
-        ]
-    },
-    {
-        "heading" : "Engineering Economics",
-        "innerdata" : [
-            ["Engineering Economics","https://drive.google.com/open?id=1RfBgu_QcDdEz3NPmk-KCZGkdMmOXQEaE"],
-            ["Selected Notes - A. M. Clausing","https://drive.google.com/open?id=17n5IKZDydvZiPC4wWZu9i_GnsehcfS1k"],
-            ["Engineering Economics Text Book","https://drive.google.com/open?id=1pc9vpRUd3TCoB9JsDpw6-dj7cR30fZ_5"],
-            ["Impacts of Inflation on Capital Budgeting Decicions","https://drive.google.com/open?id=1b3qvtIW26TOW2YW6gFD5KIrbAodPVJVt"]
-        ]
-    },
-    {
         "heading" : "Propagation & Antenna",
         "innerdata" : [
             ["Antenna & Wave Propagation by A.K-Gautam","https://drive.google.com/open?id=14RpJOOJ55rNZRLKdIuMygZrRe806AyyL"],
@@ -188,6 +192,13 @@ let data = [
             ["Signals And Systems - Oppenheim","https://drive.google.com/open?id=1cyjwI6zUsey60Uw8TXErvoN-26m_ew2V"],
             ["Signals and Systems - Oppenheim Solutions","https://drive.google.com/open?id=1gSfhkTC4LHBJNWF5Kad7QqBZ1FyyEidw"],
             ["Signals and Systems, 2ed - A.V.Oppenheim & A.S.Willsky","https://drive.google.com/open?id=13iqhMg1Ns9qqrqIoiJUVcboytgICriPV"]
+        ]
+    },
+    {
+        "heading" : "Thermodynamics",
+        "innerdata" : [
+            ["Heat Transfer - A Practical Approach","https://drive.google.com/open?id=1to07gF31s2rPSxKUWJkBPgnWA83cpHUC"],
+            ["Thermodynamics By Yunus And Michael","https://drive.google.com/open?id=1qPebXWbOxWNrduhrH3cpEOKUhvBWe6Sn"]
         ]
     }
 ]
