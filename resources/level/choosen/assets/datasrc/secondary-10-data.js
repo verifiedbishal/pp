277868d58d5075dde47fcc1493b24ed1a549d93b
Bishal Shrestha <verifiedbishal@gmail.com>
2020-05-09 11:13:11 +0545

    Added one file

--- resources/level/choosen/assets/datasrc/secondary-10-data.js
+++ resources/level/choosen/assets/datasrc/secondary-10-data.js
@@ -4,7 +4,8 @@ let data = [
         "innerdata" : [
             ["Speedy Maths 10","https://drive.google.com/open?id=18bLXkw2ZhNnLSWcl3sFPyKsko8wU9gl9"],
             ["Speedy Optional Maths 10","https://drive.google.com/open?id=1yoE_JM_N8uHyYFn9gFavoTu7Uw27yBA1"],
-            ["Science (Courtesy : Khullakitab)","https://drive.google.com/open?id=1s0QY5ZU2k_VCJHBmWZQ2o-oPzkYeoU1g"]
+            ["Science (edited)","https://drive.google.com/open?id=1s0QY5ZU2k_VCJHBmWZQ2o-oPzkYeoU1g"],
+            ["EPH (edited)","https://drive.google.com/open?id=1IEkDuLtzs_ODvldXZVeQQLXyAz0ADL1H"]
         ]
     },
     {
